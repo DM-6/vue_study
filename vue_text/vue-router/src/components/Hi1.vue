cfcd0e2203acfc543c4d71600521544ff57d4bf6
DM-6 <1063664987@qq.com>
2018-07-22 18:16:13 +0800

    vue-router入门 单页面多区域运用操作

---
+++ vue_text/vue-router/src/components/Hi1.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>{{ message }}</h2>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            message: 'I am Hi1'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue_text/vue-router/src/components/Hi2.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>{{ message }}</h2>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            message: 'I am Hi2'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
