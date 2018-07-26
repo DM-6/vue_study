4448f067a2ee3c049dc6b143dc31b1804110aba9
DM-6 <1063664987@qq.com>
2018-07-27 00:04:23 +0800

    redirect重定向 alias别名  路由的过渡动画 mode:history/hash 404页面处理

--- vue_text/vue-router/src/App.vue
+++ vue_text/vue-router/src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png"><br>
-    <router-link to="/">Home</router-link>
-    <router-link to="/params/198/DM website is very good!">params</router-link>
-    <router-view/>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/params/198/DM website is very good!">params</router-link> |
+    <router-link to="/goHome">goHome</router-link> |
+    <router-link to="/goParams/918/i like vue.js">goParams</router-link> |
+    <router-link to="/hi1">hi1</router-link> |
+    <router-link to="/dm">dm</router-link> |
+    <router-link to="345">测试</router-link> |
+
+    <transition name="fade" mode="out-in">
+      <router-view/>
+    </transition>
+    
     </div>
 </template>
 
@@ -21,5 +30,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.fade-enter{
+  opacity: 0;
+}
+.fade-enter-active{
+  transition: opacity .5s;
+}
+.fade-leave{
+  opacity: 1;
+}
+.fade-leave-active{
+  opacity: 0;
+  transition: opacity .5s;
 }
 </style>
--- vue_text/vue-router/src/router/index.js
+++ vue_text/vue-router/src/router/index.js
@@ -2,18 +2,34 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Params from '@/components/params'
+import Hi1 from '@/components/hi1'
+import Error from '@/components/error'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'hash',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
-      components: HelloWorld
+      component: HelloWorld
     },{
-      path: '/params/:newsId/:newsTitle',
+      path: '/params/:newsId(\\d+)/:newsTitle',
       component: Params
+    },{
+      path: '/goHome',
+      redirect: '/'
+    },{
+      path: '/goParams/:newsId(\\d+)/:newsTitle',
+      redirect: '/params/:newsId(\\d+)/:newsTitle'
+    },{
+      path: '/hi1',
+      component: Hi1,
+      alias: '/dm'
+    },{
+      path: '*',
+      component: Error
     }
   ]
 })
